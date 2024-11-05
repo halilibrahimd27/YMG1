524349e3c1eaf8eb3c984e199df52fb093eecb87
SemihTemur <94394628+Semih1907temur@users.noreply.github.com>
2024-11-05 19:18:43 +0300

    GET /add endpoint'i eklendi,iki sayıyı toplama işlevi eklendi.

--- api.py
+++ api.py
@@ -10,6 +10,19 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/')
 
+class Add(Resource):
+    def get(self):
+        num1 = request.args.get('num1', type=int)
+        num2 = request.args.get('num2', type=int)
+        
+        if num1 is None or num2 is None:
+            return {"message": "Hem num1 hem de num2 tam sayı olarak gereklidir."}, 400
+
+        result = num1 + num2
+        return {"result": result}, 200
+
+api.add_resource(Add, '/add')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
