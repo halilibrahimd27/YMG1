8205906d983a0e6f189a4b5c813f426f4f3192e7
halilibrahimd27 <s.ibrahimdrms@gmail.com>
2024-12-25 01:03:05 +0300

    POST /multiply endpointinde veri kontrolü eklendi

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
@@ -24,11 +24,11 @@ class Add(Resource):
 class Multiply(Resource):
     def post(self):
         data = request.get_json()
-        num1 = data.get("num1")
-        num2 = data.get("num2")
-
-        if num1 is None or num2 is None:
-            return {"message": "JSON gövdesinde hem num1 hem de num2 gereklidir."}, 400
+        try:
+            num1 = int(data.get("num1"))
+            num2 = int(data.get("num2"))
+        except (TypeError, ValueError):
+            return {"message": "Hem num1 hem de num2 tam sayı olmalıdır."}, 400
         
         result = num1 * num2
         return {"result": result}, 200
@@ -37,6 +37,11 @@ api.add_resource(HelloWorld, '/')
 api.add_resource(Add, '/add')
 api.add_resource(Multiply, '/multiply')
 
-
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=27)
+    app.run(host="127.0.0.1", port=2700)
+
+
+# Örnek kullanım
+# curl "http://127.0.0.1:2700/add?num1=5&num2=10"
+# curl -X POST -H "Content-Type: application/json" -d '{"num1": 3, "num2": 4}' http://127.0.0.1:2700/multiply
+# kullanımlar Postman aracılığı ile yapılabilir
